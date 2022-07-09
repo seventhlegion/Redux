2878eb52c30c35272d6ef2e9119f7942d4572611
Ali <ali.ahmadi.1377@hotmail.com>
2022-07-09 15:40:35 +0430

    Global style created and components added into app.js

---
+++ public/Data/Foods.js
@@ -0,0 +1,12 @@
+export const Foods = ({
+    id: 1,
+    type: 'soup',
+    payload: {
+        name: 'Milk Soup',
+        descriptons: '',
+        const: 10.5,
+    },
+    
+})
+
+export default Foods;
