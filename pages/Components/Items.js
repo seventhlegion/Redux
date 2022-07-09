899f818b63e2b608f303b9032a8e986a302747b0
Ali <ali.ahmadi.1377@hotmail.com>
2022-07-09 15:39:17 +0430

    items added and styles added

---
+++ pages/Components/Items.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { StyledItems } from './Style/Items.style'
+
+export default function Items() {
+  return (
+    <StyledItems>
+        Hey
+    </StyledItems>
+  )
+}
