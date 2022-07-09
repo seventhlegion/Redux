8b101221e5681e0fa8d4d03ec35b98959bb59295
Ali <ali.ahmadi.1377@hotmail.com>
2022-07-09 16:40:52 +0430

    Dataset Created

--- pages/Components/Items.js
+++ pages/Components/Items.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import { StyledItems } from './Style/Items.style'
+import { StyledItems } from './Style/Items.style';
+import Foods from '../../public/Data/Foods';
 
 export default function Items() {
   return (
     <StyledItems>
-        Hey
+        <div>
+        </div>
     </StyledItems>
   )
 }
--- public/Data/Foods.js
+++ public/Data/Foods.js
@@ -1,12 +1,49 @@
-export const Foods = ({
-    id: 1,
-    type: 'soup',
-    payload: {
-        name: 'Milk Soup',
-        descriptons: '',
-        const: 10.5,
+export const Foods = ([
+    {
+        id: 1,
+        type: 'soup',
+        payload: {
+            name: 'Milk Soup',
+            descriptons: '',
+            cost: 10.5,
+        }
     },
-    
-})
+        {
+        id: 2,
+        type: 'soup',
+        payload: {
+            name: 'barley soup',
+            discriptions: '',
+            cost: 9.5,
+        }
+    },
+        {
+        id: 3,
+        type: 'soup',
+        payload: {
+            name: 'Cream soup',
+            discriptions: '',
+            cost: 15.2,
+        }
+    },
+    {
+        id: 4,
+        type: 'soup',
+        payload: {
+            name: 'Puree soup',
+            discriptions: '',
+            cost: 17.2,
+        }
+    },
+    {
+        id: 5,
+        type: 'soup',
+        payload: {
+            name: 'Bisque soup',
+            discriptions: '',
+            cost: 7.2,
+        }
+    },
+])
 
 export default Foods;
