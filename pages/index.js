a308d5d833cbff1005a1506593783249e2368320
Ali Ahmadi <ali.ahmadi.1377@hotmail.com>
2022-07-12 21:28:15 +0430

    rootReducer and store created for redux

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { useState } from "react";
 import { ThemeProvider } from "styled-components";
+import { StyledContainer } from "./Components/Style/Container.style";
 import All from "./Components/All";
 import Appetiziers from "./Components/Appetiziers";
 import Filter from "./Components/Filter";
@@ -8,38 +9,47 @@ import Desserts from "./Components/Desserts";
 import Salads from "./Components/Salads";
 import Nav from "./Components/Nav";
 import Soup from "./Components/Soup";
-import { StyledContainer } from "./Components/Style/Container.style";
-import Global from './Components/Style/Global';
+import Global from "./Components/Style/Global";
 import Main from "./Components/main";
+import { INITIAL_STATE } from "./Components/Shopping/Shopping.reducer";
+import store from "./Components/Redux/store";
 
 const theme = {
-  bg: '#131d1b',
-  fc: '#cc5050',
-}
+  bg: "#131d1b",
+  fc: "#cc5050",
+};
 
+store.subscribe(() => {
+  console.log("Store Changed !" && store.getState());
+});
 
+console.log(store.getState());
 
 export default function Home() {
+  const [click, setClick] = useState("all");
+  const [add, setAdd] = useState([{}]);
 
-  const [click, setClick] = useState('all')
+  INITIAL_STATE({ add });
+
+  console.log(add);
 
   return (
     <ThemeProvider theme={theme}>
       <Head>
-        <link rel="icon" href='/pics/carrot-fill.png' />
+        <link rel="icon" href="/pics/carrot-fill.png" />
         <title>Carrot Marrot</title>
       </Head>
-    <Global />
-    <Nav />
-    <StyledContainer>
-    <Filter setClick={setClick} />
-    {click === 'all'? <All />: ''}
-    {click === 'soups'? <Soup />: ''}
-    {click === 'desserts'? <Desserts />: ''}
-    {click === 'appetiziers'? <Appetiziers />: ''}
-    {click === 'salads'? <Salads />: ''}
-    {click === 'main'? <Main />: ''}
-    </StyledContainer>
+      <Global />
+      <Nav />
+      <StyledContainer>
+        <Filter setClick={setClick} />
+        {click === "all" ? <All addToCart={setAdd} /> : ""}
+        {click === "soups" ? <Soup addToCart={setAdd} /> : ""}
+        {click === "desserts" ? <Desserts addToCart={setAdd} /> : ""}
+        {click === "appetiziers" ? <Appetiziers addToCart={setAdd} /> : ""}
+        {click === "salads" ? <Salads addToCart={setAdd} /> : ""}
+        {click === "main" ? <Main addToCart={setAdd} /> : ""}
+      </StyledContainer>
     </ThemeProvider>
-  )
+  );
 }
