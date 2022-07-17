9617fbd74561adf72c814f7697214af87e857243
Ali Ahmadi <ali.ahmadi.1377@hotmail.com>
2022-07-17 16:43:09 +0430

    Deleted

--- pages/Components/Redux/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import shopReducer from "../Shopping/Shopping.reducer";
-
-const rootReducer = combineReducers({
-  shop: shopReducer,
-});
-
-export default rootReducer;
